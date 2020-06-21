eb3b897707ba32f46de566cabba7768be0fb48d0
stefano <rquattrogtl@gmail.com>
2020-06-21 21:51:59 +0200

    Ho cambiato l'HTML per questo sito.

---
+++ my_blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+
+    url(r'^$', views.post_list, name='post_list'),
+
+]
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    path(r'', include('blog.urls')),
 ]
